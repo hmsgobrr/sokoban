d353ba0c32302f65d4ce5cd4dfe93f4067e281e3
hmsgobrr <metalcorehms@gmail.com>
2024-06-10 05:32:09 +0700

    workingonwindows

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,49 @@
 #include "glad/glad.h"
-#include <GLFW/glfw.h>
+#include <GLFW/glfw3.h>
+#include <cstdio>
 
-#include <iostream>
+void glfw_errorcallback(int error, const char* desc) {
+    fprintf(stderr, "GLFW ERR [%d]: %s", error, desc);
+}
+
 
 int main() {
-    glfwInit();
+    glfwSetErrorCallback(glfw_errorcallback);
+    
+    if (!glfwInit()) return -1;
+
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
     GLFWwindow* window = glfwCreateWindow(800, 600, "titol", NULL, NULL);
     if (!window) {
-        cout << "glfw window init failed" << endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        fprintf(stderr, "GLAD ERR: init failed");
+        glfwTerminate();
         return -1;
     }
 
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-        glfwTerminate();
-        cout << "GLAD init failed" << endl;
-        return;
+    int glvers[2] = { -1, -1 };
+    glGetIntegerv(GL_MAJOR_VERSION, &glvers[0]);
+    glGetIntegerv(GL_MINOR_VERSION, &glvers[1]);
+    printf("Using OpenGL %d.%d", glvers[0], glvers[1]);
+
+    glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
     }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
 }
 
